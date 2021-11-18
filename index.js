c7d233fdac679597be7f3ab107143361c1717404
webmakersites <73564308+webmakersites@users.noreply.github.com>
2021-11-18 12:59:40 -0600

    Update index.js

--- index.js
+++ index.js
@@ -50,3 +50,29 @@ function closeNav() {
   document.getElementById("main").style.marginLeft= "0";
   document.body.style.backgroundColor = "white";
 }
+/* WEB SHARE API */
+var shareObj = {
+  text: (document.title),
+    url: (window.location.href)
+
+},
+shareItem = {
+    init: function(obj){
+        if(navigator.share){
+            console.log('Web Share API is Supported');
+            navigator.share(obj).then(function(){
+                console.log('Shared!', obj);
+            }).catch(function(error){
+                console.log('Not Shared! ', error);
+              document.querySelector('#result').innerText = error;
+            });
+        }else{
+            console.log('Web Share API is not Supported');
+        }
+    }
+};
+
+document.querySelector('.btn-share').addEventListener('click', function(e){
+  e.preventDefault();
+  shareItem.init(shareObj);
+});
